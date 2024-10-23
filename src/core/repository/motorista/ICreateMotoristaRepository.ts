d68f9f52218085c3cb1df1d41293a63768159ef1
Afonso Barbosa <afonso33barbosa@gmail.com>
2024-10-23 15:15:21 -0400

    adicionado rinterfaces de contrato do motorista

---
+++ src/core/repository/motorista/ICreateMotoristaRepository.ts
@@ -0,0 +1,9 @@
+import type { typeMotorista } from "../../types";
+import { Motorista } from "../../entities/Motorista";
+
+export interface ICreateMotoristaRepository{
+
+  verifyMotorista(cpf: string): Promise<typeMotorista>;
+
+  createMotorista(data: Motorista): Promise<number>;
+}
---
+++ src/core/repository/motorista/IUpdateMotoristaRepository.ts
@@ -0,0 +1,4 @@
+export interface IUpdateMotoristaRepository{
+
+  updateMotorista():Promise<number>;
+}
