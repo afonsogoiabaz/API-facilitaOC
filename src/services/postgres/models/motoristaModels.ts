b6c0737e8e58236af2341030ce18a5c01c1b31a9
Afonso Barbosa <afonso33barbosa@gmail.com>
2024-10-30 13:47:36 -0400

    adicionado rota de update motorista

--- src/services/postgres/models/motoristaModels.ts
+++ src/services/postgres/models/motoristaModels.ts
@@ -1,3 +1,4 @@
+import type { updateMotoristaRequestDTO } from '../../../core/DTO/motorista/updateMotoristaRequestDTO';
 import type { Motorista } from '../../../core/entities/Motorista';
 import * as db from '../pgIndex';
 
@@ -6,7 +7,9 @@ export class MotoristaModels{
   createMotorista = async(motorista: Motorista)=>{
     return(
       await db.query(
-        "insert into motorista(nome_motorista, cpf_motorista, rg_motorista, orgaoexpeditor_motorista, telefone_motorista) values ($1, $2, $3, $4, $5)", 
+        `insert into motorista
+          (nome_motorista, cpf_motorista, rg_motorista, orgaoexpeditor_motorista, telefone_motorista) 
+        values ($1, $2, $3, $4, $5)`, 
         [
           motorista.nome,
           motorista.cpf,
@@ -37,5 +40,22 @@ export class MotoristaModels{
         [cpf]
       )
     );
-  } 
+  };
+
+  updateMotorista = async(id: string, data: updateMotoristaRequestDTO)=>{
+    return(
+      await db.query(
+        `update motorista set
+          nome_motorista = $1, rg_motorista = $2, orgaoexpeditor_motorista = $3, telefone_motorista = $4
+        where motorista.id_motorista = $5`,
+        [
+          data.nome,
+          data.rg,
+          data.orgaoexpeditor,
+          data.telefone,
+          id
+        ]
+      )
+    );
+  };
 }
